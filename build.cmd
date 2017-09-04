35c1fb3f86128fea70ced70c579dc10b6ed5d2e0
Unknown <trevor.sandy@gmail.com>
2017-09-04 19:24:06 +0200

    Update config files for Windows

--- build.cmd
+++ build.cmd
@@ -1,4 +1,4 @@
-@ECHO off
+@ECHO OFF
 
 Title LDView Windows auto build script
 
@@ -8,7 +8,7 @@ Title LDView Windows auto build script
 :: resources ) as LPub3D 3rd Party components.
 :: --
 ::  Trevor SANDY <trevor.sandy@gmail.com>
-::  Last Update: July 07, 2017
+::  Last Update: September 04, 2017
 ::  Copyright (c) 2017 by Trevor SANDY
 :: --
 :: This script is distributed in the hope that it will be useful,
@@ -21,11 +21,13 @@ SET PWD=%~dp0
 SET APPNAME=LDView
 SET VERSION=4.3
 SET LDRAW_DIR=%USERPROFILE%\LDraw
-SET DIST_DIR_ROOT=..\lpub3d-windows-3rdparty
-SET INI_FILE="%PWD%\OSMesa\LDViewCustomIni"
+SET DIST_DIR_ROOT=..\lpub3d_windows_3rdparty
+SET INI_POV_FILE=%PWD%\OSMesa\ldviewPOV.ini
 SET CONFIGURATION=Release
 SET PLATFORM=unknown
+SET INI_FILE=unknown
 SET THIRD_INSTALL=unknown
+SET CHECK=unknown
 
 :: Check if invalid platform flag
 IF NOT [%1]==[] (
@@ -71,6 +73,7 @@ IF NOT [%2]==[] (
 
 :: Parse configuration input flag
 IF [%2]==[] (
+  SET THIRD_INSTALL=1
   GOTO :BUILD
 )
 
@@ -90,8 +93,14 @@ IF /I "%2"=="-chk" (
 :: Note you can change this line to your specific environment - I am using VS2017 here.
 CALL "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\VsDevCmd.bat"
 
+:: Backup ini files
+CALL :BACKUP_INI_FILES
+
 :: If check specified, update inifile with ldraw directory path
-IF %CHECK%==1 CALL :UPDATE_INI_FILE
+CALL :UPDATE_INI_POV_FILE
+
+:: Console logging flags (see https://docs.microsoft.com/en-us/visualstudio/msbuild/msbuild-command-line-reference)
+SET LOGGING=/clp:ErrorsOnly /nologo
 
 :: Check if build all platforms
 IF /I "%PLATFORM%"=="-all" (
@@ -102,13 +111,17 @@ IF /I "%PLATFORM%"=="-all" (
 ECHO.
 ECHO -Building %PLATFORM% Platform...
 ECHO.
-
+:: Assemble command line
+SET COMMAND_LINE=msbuild /m /p:Configuration=%CONFIGURATION% /p:Platform=%PLATFORM% LDView.vcxproj %LOGGING% 
+ECHO -Command: %COMMAND_LINE%
 :: Launch msbuild
-msbuild /m /p:Configuration=%CONFIGURATION% /p:Platform=%PLATFORM% LDView.vcxproj
+%COMMAND_LINE%  
 :: Perform build check if specified
 IF %CHECK%==1 CALL :CHECK_BUILD %PLATFORM%
 :: Package 3rd party install
-IF %THIRD_INSTALL%==1 GOTO :3RD_PARTY_INSTALL
+IF %THIRD_INSTALL%==1 CALL :3RD_PARTY_INSTALL
+:: Restore ini file 
+CALL :RESTORE_INI_FILES
 :: Finish
 EXIT /b
 
@@ -118,13 +131,79 @@ FOR %%P IN ( Win32, x64 ) DO (
   ECHO.
   ECHO -All Platforms: Building %%P Platform...
   ECHO.
-  msbuild /m /p:Configuration=%CONFIGURATION% /p:Platform=%%P LDView.vcxproj
+  :: Assemble command line
+  SET COMMAND_LINE=msbuild /m /p:Configuration=%CONFIGURATION% /p:Platform=%%P LDView.vcxproj %LOGGING%
+  SETLOCAL ENABLEDELAYEDEXPANSION
+  ECHO -Command: !COMMAND_LINE!
+  !COMMAND_LINE!
+  ENDLOCAL
   :: Perform build check if specified
   IF %CHECK%==1 CALL :CHECK_BUILD %%P
 )
+:: Restore ini file 
+CALL :RESTORE_INI_FILES
+EXIT /b
+
+:UPDATE_INI_POV_FILE
+IF EXIST "%LDRAW_DIR%\lgeo" (
+SET /a LineToReplace=50
+SET "Replacement=XmlMapPath=%LDRAW_DIR%\lgeo"
+ECHO.
+SETLOCAL ENABLEDELAYEDEXPANSION
+ECHO -Update %INI_POV_FILE% at line !LineToReplace! with !Replacement!
+ENDLOCAL
+(FOR /f "tokens=1*delims=:" %%a IN ('findstr /n "^" "%INI_POV_FILE%"') DO (
+  SET "Line=%%b"
+  IF %%a equ %LineToReplace% SET "Line=%Replacement%"
+    SETLOCAL ENABLEDELAYEDEXPANSION
+    ECHO(!Line!
+    ENDLOCAL
+))>"%INI_POV_FILE%.new"
+MOVE /Y "%INI_POV_FILE%.new" "%INI_POV_FILE%" >nul 2>&1
+) ELSE (
+	ECHO.
+	ECHO -LGEO directory %LDRAW_DIR%\lgeo does not exist. 
+	ECHO -Update aborted.
+)
+EXIT /b
+
 :: Perform 3rd party install if specified
-IF %THIRD_INSTALL%==1 GOTO :3RD_PARTY_INSTALL
+IF %THIRD_INSTALL%==1 CALL :3RD_PARTY_INSTALL
+:: Restore ini file 
+CALL :RESTORE_INI_FILES
 :: Finish
+EXIT /b
+
+:CHECK_BUILD
+CALL :CHECK_LDRAW_DIR
+IF %CHECK%==1 (
+SET INI_FILE=%INI_POV_FILE%
+IF %1==Win32 SET PL=
+IF %1==x64 SET PL=64
+IF EXIST "8464.TestResult.%1.png" (
+	DEL /Q "8464.TestResult.%1.png"
+)
+SETLOCAL ENABLEDELAYEDEXPANSION
+ECHO.
+ECHO -Check %CONFIGURATION% Configuration, %1 Platform using Ini file !INI_FILE!...
+ECHO.
+SET COMMAND_LINE=Build\release%PL%\%APPNAME%%PL%.exe "8464.mpd" -SaveSnapshot="8464.TestResult.%1.png" -IniFile=!INI_FILE! -SaveWidth=128 -SaveHeight=128 -ShowErrors=0 -SaveActualSize=0
+ECHO -Command: !COMMAND_LINE! 
+!COMMAND_LINE! 
+ENDLOCAL
+)
+EXIT /b
+
+:CHECK_LDRAW_DIR
+ECHO.
+ECHO -Check requested.
+IF NOT EXIST %LDRAW_DIR% (
+	SET CHECK=0
+	ECHO.
+	ECHO -LDraw directory %LDRAW_DIR% does not exist.
+	ECHO -Check aborted.
+	EXIT /b
+)
 EXIT /b
 
 :3RD_PARTY_INSTALL
@@ -132,87 +211,59 @@ ECHO.
 ECHO -Copying 3rd party distribution files...
 ECHO.
 ECHO -Copying 32bit exe...
-IF NOT EXIST "%DIST_DIR_ROOT%\bin\%APPNAME%-%VERSION%\i386\" (
-  MKDIR "%DIST_DIR_ROOT%\bin\%APPNAME%-%VERSION%\i386\"
+IF NOT EXIST "%DIST_DIR_ROOT%\%APPNAME%-%VERSION%\bin\i386\" (
+  MKDIR "%DIST_DIR_ROOT%\%APPNAME%-%VERSION%\bin\i386\"
 )
-COPY /V /Y "Build\Release\%APPNAME%.exe" "%DIST_DIR_ROOT%\bin\%APPNAME%-%VERSION%\i386\" /B
+COPY /V /Y "Build\Release\%APPNAME%.exe" "%DIST_DIR_ROOT%\%APPNAME%-%VERSION%\bin\i386\" /B
 
 ECHO -Copying 64bit exe...
-IF NOT EXIST "%DIST_DIR_ROOT%\bin\%APPNAME%-%VERSION%\x86_64\" (
-  MKDIR "%DIST_DIR_ROOT%\bin\%APPNAME%-%VERSION%\x86_64\"
+IF NOT EXIST "%DIST_DIR_ROOT%\%APPNAME%-%VERSION%\bin\x86_64\" (
+  MKDIR "%DIST_DIR_ROOT%\%APPNAME%-%VERSION%\bin\x86_64\"
 )
-COPY /V /Y "Build\Release64\%APPNAME%64.exe" "%DIST_DIR_ROOT%\bin\%APPNAME%-%VERSION%\x86_64\" /B
+COPY /V /Y "Build\Release64\%APPNAME%64.exe" "%DIST_DIR_ROOT%\%APPNAME%-%VERSION%\bin\x86_64\" /B
 
 ECHO -Copying Documentaton...
-IF NOT EXIST "%DIST_DIR_ROOT%\docs\%APPNAME%-%VERSION%\" (
-  MKDIR "%DIST_DIR_ROOT%\docs\%APPNAME%-%VERSION%\"
+IF NOT EXIST "%DIST_DIR_ROOT%\%APPNAME%-%VERSION%\docs\" (
+  MKDIR "%DIST_DIR_ROOT%\%APPNAME%-%VERSION%\docs\"
 )
-SET DIST_DIR="%DIST_DIR_ROOT%\docs\%APPNAME%-%VERSION%\"
+SET DIST_DIR=%DIST_DIR_ROOT%\%APPNAME%-%VERSION%\docs\
 COPY /V /Y "Readme.txt" "%DIST_DIR%" /A
 COPY /V /Y "Help.html" "%DIST_DIR%" /A
 COPY /V /Y "license.txt" "%DIST_DIR%" /A
 COPY /V /Y "ChangeHistory.html" "%DIST_DIR%" /A
 
-ECHO -Copying Resources...
-IF NOT EXIST "%DIST_DIR_ROOT%\resources\%APPNAME%-%VERSION%\" (
-  MKDIR "%DIST_DIR_ROOT%\resources\%APPNAME%-%VERSION%\"
+ECHO -Copying Ini Files...
+IF NOT EXIST "%DIST_DIR_ROOT%\%APPNAME%-%VERSION%\resources\config\" (
+  MKDIR "%DIST_DIR_ROOT%\%APPNAME%-%VERSION%\resources\config\"
 )
-SET DIST_DIR="%DIST_DIR_ROOT%\resources\%APPNAME%-%VERSION%\"
+SET DIST_DIR=%DIST_DIR_ROOT%\%APPNAME%-%VERSION%\resources\config
+COPY /V /Y "OSMesa\LDViewCustomIni" "%DIST_DIR%" /A
+COPY /V /Y "OSMesa\ldview.ini" "%DIST_DIR%" /A
+COPY /V /Y "OSMesa\ldviewPOV.ini" "%DIST_DIR%" /A
+
+ECHO -Copying Resources...
+SET DIST_DIR=%DIST_DIR_ROOT%\%APPNAME%-%VERSION%\resources\
 COPY /V /Y "m6459.ldr" "%DIST_DIR%" /A
 COPY /V /Y "8464.mpd" "%DIST_DIR%" /A
-COPY /V /Y "OSMesa\LDViewCustomIni" "%DIST_DIR%" /A
-COPY /V /Y "OSMesa\LDViewCustomIni" "%DIST_DIR%" /A
-:: Finish
+COPY /V /Y "LDExporter\LGEO.xml" "%DIST_DIR%" /A
 EXIT /b
 
-:CHECK_BUILD
-SET PWD=%~dp0
-IF %1==Win32 SET PL=
-IF %1==x64 SET PL=64
+:BACKUP_INI_FILES
 ECHO.
-ECHO -Check %CONFIGURATION% Configuration, %1 Platform...
-ECHO.
-ECHO -Command: %APPNAME%%PL%.exe "8464.mpd" -SaveSnapshot="8464.TestResult.%1.png" -IniFile=%INI_FILE% -SaveWidth=128 -SaveHeight=128 -ShowErrors=0 -SaveActualSize=0
-IF EXIST "8464.TestResult.%1.png" (
-	DEL /Q "8464.TestResult.%1.png"
-)
-Build\release%PL%\%APPNAME%%PL%.exe "8464.mpd" -SaveSnapshot="8464.TestResult.%1.png" -IniFile=%INI_FILE% -SaveWidth=128 -SaveHeight=128 -ShowErrors=0 -SaveActualSize=0
+ECHO -Backing up ini files...
+SET INI_FILE=%PWD%\OSMesa\ldviewPOV.ini
+COPY /V /Y "%INI_FILE%" "%INI_FILE%.hold" /A
+SET INI_FILE=%PWD%\OSMesa\ldview.ini
+COPY /V /Y "%INI_FILE%" "%INI_FILE%.hold" /A
 EXIT /b
 
-:UPDATE_INI_FILE
-IF NOT EXIST %LDRAW_DIR% (
-	SET CHECK=0
-	ECHO.
-	ECHO -LDraw directory %LDRAW_DIR% does not exist.
-	ECHO -Check aborted.
-	ECHO.
-	EXIT /b
-)
+:RESTORE_INI_FILES
 ECHO.
-ECHO -Check requested.
-SET /a LineToReplace=31
-SET "Replacement=LDrawDir=%LDRAW_DIR%"
-ECHO - Upating %INI_FILE% at line %LineToReplace% with %Replacement%
-(FOR /f "tokens=1*delims=:" %%a IN ('findstr /n "^" "%INI_FILE%"') DO (
-  SET "Line=%%b"
-  IF %%a equ %LineToReplace% SET "Line=%Replacement%"
-    SETLOCAL ENABLEDELAYEDEXPANSION
-    ECHO(!Line!
-    ENDLOCAL
-))>"%INI_FILE%.new"
-MOVE /Y %INI_FILE%.new %INI_FILE% >nul 2>&1
-SET /a LineToReplace=57
-SET "Replacement=XmlMapPath=%LDRAW_DIR%\lgeo"
-ECHO - Upating %INI_FILE% at line %LineToReplace% with %Replacement%
-(FOR /f "tokens=1*delims=:" %%a IN ('findstr /n "^" "%INI_FILE%"') DO (
-  SET "Line=%%b"
-  IF %%a equ %LineToReplace% SET "Line=%Replacement%"
-    SETLOCAL ENABLEDELAYEDEXPANSION
-    ECHO(!Line!
-    ENDLOCAL
-))>"%INI_FILE%.new"
-MOVE /Y %INI_FILE%.new %INI_FILE% >nul 2>&1
-ECHO.
+ECHO -Restoring ini files...
+SET INI_FILE=%PWD%\OSMesa\ldviewPOV.ini
+MOVE /Y "%INI_FILE%.hold" "%INI_FILE%" >nul 2>&1
+SET INI_FILE=%PWD%\OSMesa\ldview.ini
+MOVE /Y "%INI_FILE%.hold" "%INI_FILE%" >nul 2>&1
 EXIT /b
 
 :PLATFORM_ERROR
