fa08ea2e563b87b46e2ccbe9bf8385d192b01187
Trevor SANDY <trevor.sandy@gmail.com>
2021-07-01 16:47:25 +0200

    Restore platform variable

--- build.cmd
+++ build.cmd
@@ -15,7 +15,7 @@ rem This script is distributed in the hope that it will be useful,
 rem but WITHOUT ANY WARRANTY; without even the implied warranty of
 rem MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 
-CALL :LDV_ELAPSED_BUILD_TIME Start
+CALL :ELAPSED_BUILD_TIME Start
 
 SET PWD=%CD%
 
@@ -98,7 +98,7 @@ SET MINIMUM_LOGGING=unknown
 SET THIRD_INSTALL=unknown
 SET INSTALL_32BIT=unknown
 SET INSTALL_64BIT=unknown
-SET PLATFORM_ARCH=unknown
+SET PLATFORM=unknown
 SET INI_FILE=unknown
 SET CHECK=unknown
 
@@ -118,19 +118,19 @@ IF NOT [%1]==[] (
 
 rem Parse platform input flag
 IF [%1]==[] (
-  SET PLATFORM_ARCH=-all
+  SET PLATFORM=-all
   GOTO :SET_CONFIGURATION
 )
 IF /I "%1"=="x86" (
-  SET PLATFORM_ARCH=Win32
+  SET PLATFORM=Win32
   GOTO :SET_CONFIGURATION
 )
 IF /I "%1"=="x86_64" (
-  SET PLATFORM_ARCH=x64
+  SET PLATFORM=x64
   GOTO :SET_CONFIGURATION
 )
 IF /I "%1"=="-all" (
-  SET PLATFORM_ARCH=-all
+  SET PLATFORM=-all
   GOTO :SET_CONFIGURATION
 )
 IF /I "%1"=="-help" (
@@ -178,7 +178,6 @@ IF /I "%2"=="-chk" (
 
 :BUILD
 rem Display build settings
-ECHO.
 IF "%GITHUB%" EQU "True" (
   ECHO   BUILD_HOST.............[GITHUB CONTINUOUS INTEGRATION SERVICE]
   ECHO   BUILD_WORKER_IMAGE.....[%GITHUB_RUNNER_IMAGE%]
@@ -233,13 +232,13 @@ rem If check specified, update inifile with ldraw directory path
 CALL :UPDATE_INI_POV_FILE
 
 rem Check if build all platforms
-IF /I "%PLATFORM_ARCH%"=="-all" (
+IF /I "%PLATFORM%"=="-all" (
   GOTO :BUILD_ALL
 )
 
 rem Check if build Win32 and vs2019, set to vs2017 for WinXP compat
 IF "%LP3D_VSVERSION%"=="2019" (
-  CALL :CONFIGURE_VCTOOLS %PLATFORM_ARCH%
+  CALL :CONFIGURE_VCTOOLS %PLATFORM%
 )
 
 rem Initialize the Visual Studio command line development environment
@@ -247,19 +246,19 @@ CALL :CONFIGURE_BUILD_ENV
 
 rem Display platform setting
 ECHO.
-ECHO -Building %PLATFORM_ARCH% Platform...
+ECHO -Building %PLATFORM% Platform...
 ECHO.
 rem Assemble command line
-SET COMMAND_LINE=msbuild /m /p:Configuration=%CONFIGURATION% /p:Platform=%PLATFORM_ARCH% /p:WindowsTargetPlatformVersion=%LP3D_VCVERSION% /p:PlatformToolset=%LP3D_VCTOOLSET% %PROJECT% %LOGGING_FLAGS%
+SET COMMAND_LINE=msbuild /m /p:Configuration=%CONFIGURATION% /p:Platform=%PLATFORM% /p:WindowsTargetPlatformVersion=%LP3D_VCVERSION% /p:PlatformToolset=%LP3D_VCTOOLSET% %PROJECT% %LOGGING_FLAGS%
 ECHO -Command: %COMMAND_LINE%
 rem Launch msbuild
 %COMMAND_LINE%
 rem Perform build check if specified
-IF %CHECK%==1 CALL :CHECK_BUILD %PLATFORM_ARCH%
+IF %CHECK%==1 CALL :CHECK_BUILD %PLATFORM%
 rem Package 3rd party install content
 IF %THIRD_INSTALL%==1 (
-  IF %PLATFORM_ARCH%==Win32 SET INSTALL_32BIT=1
-  IF %PLATFORM_ARCH%==x64 SET INSTALL_64BIT=1
+  IF %PLATFORM%==Win32 SET INSTALL_32BIT=1
+  IF %PLATFORM%==x64 SET INSTALL_64BIT=1
   CALL :3RD_PARTY_INSTALL
 )
 rem Restore ini file
@@ -275,7 +274,7 @@ FOR %%P IN ( Win32, x64 ) DO (
   ECHO -Building %%P Platform...
   ECHO.
   rem Initialize the Visual Studio command line development environment
-  SET PLATFORM_ARCH=%%P
+  SET PLATFORM=%%P
   IF "%LP3D_VSVERSION%"=="2019" (
     CALL :CONFIGURE_VCTOOLS %%P
   )
@@ -317,15 +316,15 @@ IF %1==x64 (
   SET LP3D_VCTOOLSET=v142
 )
 ECHO.
-ECHO -Set %1 MSBuild %PLATFORM_ARCH% platform toolset to %LP3D_VCTOOLSET%
+ECHO -Set %1 MSBuild platform toolset to %LP3D_VCTOOLSET%
 EXIT /b
 
 :CONFIGURE_BUILD_ENV
 ECHO.
-ECHO -Configure %PACKAGE% %PLATFORM_ARCH% build environment...
+ECHO -Configure %PACKAGE% %PLATFORM% build environment...
 rem Set vcvars for AppVeyor or local build environments
 IF "%PATH_PREPENDED%" NEQ "True" (
-  IF %PLATFORM_ARCH% EQU Win32 (
+  IF %PLATFORM% EQU Win32 (
     ECHO.
     IF EXIST "%LP3D_VCVARSALL%\vcvars32.bat" (
       CALL "%LP3D_VCVARSALL%\vcvars32.bat" %LP3D_VCVARSALL_VER%
@@ -350,7 +349,7 @@ IF "%PATH_PREPENDED%" NEQ "True" (
   ECHO.
 ) ELSE (
   ECHO.
-  ECHO -%PLATFORM_ARCH% build environment already configured...
+  ECHO -%PLATFORM% build environment already configured...
 )
 EXIT /b
 
@@ -788,7 +787,7 @@ ECHO If no flag is supplied, 64bit platform, Release Configuration built by defa
 ECHO ----------------------------------------------------------------
 EXIT /b
 
-:LDV_ELAPSED_BUILD_TIME
+:ELAPSED_BUILD_TIME
 IF [%1] EQU [] (SET start=%build_start%) ELSE (
   IF "%1"=="Start" (
     SET build_start=%time%
@@ -818,9 +817,9 @@ ENDLOCAL
 EXIT /b
 
 :ERROR_END
-CALL :LDV_ELAPSED_BUILD_TIME
+CALL :ELAPSED_BUILD_TIME
 EXIT /b 3
 
 :END
-CALL :LDV_ELAPSED_BUILD_TIME
+CALL :ELAPSED_BUILD_TIME
 EXIT /b
