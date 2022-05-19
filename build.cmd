10235dc106e3998287ca09b3974561f418c29b3a
Trevor SANDY <trevor.sandy@gmail.com>
2022-05-19 13:29:27 +0200

    Update Copyright to 2022

--- build.cmd
+++ build.cmd
@@ -9,7 +9,7 @@ rem resources ) as LPub3D 3rd Party components.
 rem --
 rem  Trevor SANDY <trevor.sandy@gmail.com>
 rem  Last Update: July 02, 2021
-rem  Copyright (c) 2020 - 2021 by Trevor SANDY
+rem  Copyright (c) 2020 - 2022 by Trevor SANDY
 rem --
 rem This script is distributed in the hope that it will be useful,
 rem but WITHOUT ANY WARRANTY; without even the implied warranty of
