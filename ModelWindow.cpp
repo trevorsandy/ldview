90f0480207f47d1aea1bc1b7d70d5cb5f56d58fe
Travis Cobbs <20451753+tcobbs@users.noreply.github.com>
2025-09-25 21:07:58 -0700

    Win: Fix window maximize calcs on multi-monitor systems

--- ModelWindow.cpp
+++ ModelWindow.cpp
@@ -1560,7 +1560,7 @@ BOOL ModelWindow::doDialogGetMinMaxInfo(HWND hDlg, LPMINMAXINFO minMaxInfo)
 {
 	if (hDlg == hErrorWindow)
 	{
-		calcSystemSizes();
+		calcSystemSizes(hDlg);
 		minMaxInfo->ptMaxSize.x = systemMaxWidth;
 		minMaxInfo->ptMaxSize.y = systemMaxHeight;
 		minMaxInfo->ptMinTrackSize.x = scalePoints(475);
